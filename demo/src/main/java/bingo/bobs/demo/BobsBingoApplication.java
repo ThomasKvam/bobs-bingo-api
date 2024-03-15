1aab4d926cc86ae5a1d3fad34b7934be208c4793
ThomasKvam <thomas.e.h.kvam@hotmail.no>
2024-03-15 10:59:04 +0100

    Building project

---
+++ demo/src/main/java/bingo/bobs/demo/BobsBingoApplication.java
@@ -0,0 +1,13 @@
+package bingo.bobs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobsBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobsBingoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/bingo/bobs/demo/BobsBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package bingo.bobs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobsBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
