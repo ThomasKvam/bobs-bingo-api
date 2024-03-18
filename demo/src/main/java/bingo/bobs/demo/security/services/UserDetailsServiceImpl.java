c204bd42f61c88ce3b1cbbf4afe8ab0788bfe16d
stp <steventphung6@gmail.com>
2024-03-18 10:41:39 +0100

    Added security and authentication

---
+++ demo/src/main/java/bingo/bobs/demo/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package bingo.bobs.demo.security.services;
+
+
+import bingo.bobs.demo.user.User;
+import bingo.bobs.demo.user.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByUsername(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
--- demo/src/main/java/bingo/bobs/demo/user/User.java
+++ demo/src/main/java/bingo/bobs/demo/user/User.java
@@ -1,22 +1,36 @@
 package bingo.bobs.demo.user;
 
 
+import bingo.bobs.demo.role.Role;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+
+
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @Entity
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
+        })
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column
+    @NotBlank
+    @Size(max = 20)
     private String username;
 
     @Column
@@ -25,14 +39,28 @@ public class User {
     @Column
     private String lastName;
 
-    @Column
-    private String Email;
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
 
-    public User(String username, String firstName, String lastName, String email) {
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String firstName, String lastName, String email, String password) {
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
-        Email = email;
+        this.email = email;
+        this.password = password;
     }
 
     public User(int id) {
